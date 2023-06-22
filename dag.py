8a97399659a2d65cc5f95d1c166af298c93da71e
Alan Robertson <alan.robertson@sydney.edu.au>
2023-06-22 14:37:13 +1000

    The refactor begins

--- dag.py
+++ dag.py
@@ -2,51 +2,7 @@ import numpy as np
 import copy
 from utils import log
 
-class DAGNode():
-    def __init__(self, targs, edges=None, data=None, layer_num=None, slack=0, magic_state=False, cycles=1):
-        if type(targs) is int:
-            targs = [targs]
-        if edges is None:
-            edges = {}
-
-        if data is None:
-            data = ""
-
-        self.targs = targs
-        self.data = data
-        self.cycles = cycles
-        # TODO remove when we subclass 
-        if self.data == 'CNOT':
-            self.cycles = 3
-
-        # We will be filling these in once we've got an allocation
-        self.start = -1
-        self.end = -1
-        self.anc = None
-        
-        self.edges_precede = edges
-        self.edges_antecede = {}
-
-        self.non_local = len(self.targs) > 1
-        self.slack = slack
-
-        self.resolved = False
-        self.magic_state = magic_state
-
-        if layer_num is None:
-            layer_num = max(self.edges_precede[i].layer_num + 1 for i in self.edges_precede)
-        self.layer_num = layer_num
-
-    def add_antecedent(self, targ, node):
-        self.edges_antecede[targ] = node 
-
-    def __contains__(self, i):
-        return self.targs.__contains__(i)
-
-    def __repr__(self):
-        return "{}:{}".format(self.data, self.targs)
-    def __str__(self):
-        return self.__repr__()
+from dag_node import DAGNode
 
 class DAG():
     def __init__(self, n_blocks):
---
+++ instructions.py
@@ -0,0 +1,46 @@
+from dag_node import DAGNode
+
+'''
+    Base Gate Behaviours
+'''
+class MultiQubitGate(DAGNode):
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+class InPlaceGate(DAGNode):
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+class ANCGate(DAGNode):
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+class CompositionalGate(DAGNode):
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, **kwargs)
+
+'''
+    Particular Choices of Gates
+'''
+class CNOT(MultiQubitGate):
+    def __init__(self, *args, **kwargs):
+        super().__init__(self, *args, cycles=3, **kwargs)
+
+class Z(InPlaceGate):
+    pass
+
+class X(InPlaceGate):
+    pass
+
+class INIT(InPlaceGate):
+    pass
+
+'''
+    Compositional Gates
+'''
+
+class PREP(CompositionalGate):
+    pass
+
+
+
