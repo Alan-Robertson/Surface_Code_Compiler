d97d5356edf8eba166e0d48d0d2f832615f65dd3
Alan Robertson <alan.robertson@sydney.edu.au>
2023-08-07 12:36:26 +1000

    Slots in progress

---
+++ slots.py
@@ -0,0 +1,75 @@
+import bisect
+
+from qcb import SCPatch
+
+class TreeSlots():
+    '''
+        Allocator object
+    '''
+    NO_CHILDREN_ERROR = object()
+
+    def __init__(self, tree_node):
+        self.tree_node = tree_node
+        self.slots = {}
+
+    def distribute(self, symbol, child):
+        if symbol not in self.slots:
+            self.slots[symbol] = TreeSlot(symbol)
+        self.slots[symbol].distribute(child)
+
+    def alloc(self, symbol): 
+        if symbol not in self.slots:
+            return self.NO_CHILDREN_ERROR
+        return self.slots[symbol].alloc()
+    
+    def get_weight(self, symbol):
+        return self.slots[symbol].get_weight()
+
+    def exhausted(self, symbol):
+        return self.slots[symbol].exhausted()
+
+class TreeSlot():
+    '''
+        Slot for a single symbol instance
+    '''
+    def __init__(self, symbol, initial_value=0):
+        self.symbol = symbol
+        self.value = initial_value
+        self.children = set()
+        self.ordering = []
+        
+        self.weight_updated = False
+        self.last_weight = 0
+
+    def distribute(self, child):
+        if child not in self.children:
+            self.children.add(child)
+            bisect.insort(self.ordering, child, key = lambda x: x.get_weight(self.symbol))
+        else:
+            self.ordering.sort()
+        self.last_weight = self.ordering[-1].get_weight(self.symbol)
+
+    def get_weight(self):
+        # Faster to manually track this in the ordering
+        if self.weight_updated:
+            self.last_weight = max(x.get_weight(self.symbol) for x in self.ordering)
+            self.weight_updated = False
+        return self.last_weight
+
+    def alloc(self):
+        binding = TreeSlots.NO_CHILDREN_ERROR
+        while binding == TreeSlots.NO_CHILDREN_ERROR:
+            if self.exhausted():
+                return TreeSlots.NO_CHILDREN_ERROR
+       
+            allocated = self.ordering.pop()
+            binding = allocated.alloc(self.symbol)
+       
+        # Round robin, re-insert at the start
+        if not binding.exhausted(self.symbol):
+            self.ordering.insert(0, allocated)
+        self.weight_updated = True
+        return binding 
+
+    def exhausted(self):
+        return len(self.ordering) == 0
---
+++ test_tree_slots.py
@@ -0,0 +1,89 @@
+from slots import TreeSlots, TreeSlot
+import unittest
+
+
+class DummyObj():
+    def __init__(self, symbol, weight, slots):
+        self.symbol = symbol
+        self.weight = weight
+        self.slots = slots
+    
+    def get_weight(self, symbol):
+        return self.weight
+
+    def alloc(self, symbol):
+        if self.symbol != symbol:
+            return TreeSlots.NO_CHILDREN_ERROR
+        if self.slots == 0:
+            return TreeSlots.NO_CHILDREN_ERROR
+        else:
+            self.slots -= 1
+            return self
+
+    def exhausted(self, symbol):
+        return self.slots == 0
+
+    def __repr__(self):
+        return f"[{self.symbol}: {self.weight}, {self.slots}]"
+
+    def n_slots(self):
+        return self.slots
+
+class SlotTest(unittest.TestCase):
+
+    def test_no_slots(self):
+        s = TreeSlots(None)
+        assert s.alloc('TST') == TreeSlots.NO_CHILDREN_ERROR
+
+    def test_one_alloc(self):
+        s = TreeSlots(None)
+        obj = DummyObj('TST', 2, 1)
+        s.distribute('TST', obj)
+        assert s.get_weight('TST') == 2
+        assert s.alloc('TST').n_slots() == 0
+        assert s.alloc('TST') == TreeSlots.NO_CHILDREN_ERROR
+
+    def test_two_allocs(self):
+        s = TreeSlots(None)
+        obj = DummyObj('TST', 2, 2)
+        s.distribute('TST', obj)
+        assert s.get_weight('TST') == 2
+        assert s.alloc('TST').n_slots() == 1
+        assert s.alloc('TST').n_slots() == 0
+        assert s.alloc('TST') == TreeSlots.NO_CHILDREN_ERROR
+
+    def test_two_slots(self):
+        s = TreeSlots(None)
+        obj_a = DummyObj('TST', 3, 1)
+        obj_b = DummyObj('TST', 2, 2)
+        s.distribute('TST', obj_a)
+        s.distribute('TST', obj_b)
+
+        assert s.get_weight('TST') == 3
+        assert s.alloc('TST').n_slots() == 0
+        assert(len(s.slots['TST'].ordering) == 1)
+        # Slot exhausted
+        assert s.get_weight('TST') == 2
+
+    def test_nested(self):
+        s = TreeSlots(None)
+        top = TreeSlots(None)
+        obj_a = DummyObj('TST', 3, 1)
+        obj_b = DummyObj('TST', 2, 2)
+        s.distribute('TST', obj_a)
+        s.distribute('TST', obj_b)
+
+        top.distribute('TST', s)
+        assert (len(s.slots['TST'].ordering) == 2)
+
+        assert top.get_weight('TST') == 3
+        assert top.alloc('TST') == obj_a
+        print(top.alloc('TST'))
+        assert top.alloc('TST') == obj_b
+        assert(len(s.slots['TST'].ordering) == 1)
+        # Slot exhausted
+        assert top.get_weight('TST') == 2
+
+
+if __name__ == '__main__':
+    unittest.main()
