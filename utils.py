12d0ca31396ac68cc0c134c67a42e73c8ccc15ef
Alan Robertson <alan.robertson@sydney.edu.au>
2023-08-24 17:17:11 +1000

    removed a feral file creation on import

--- utils.py
+++ utils.py
@@ -2,10 +2,3 @@ from collections import deque
 from functools import partial
 
 consume = partial(deque, maxlen=0)
-
-
-_f = open("log.txt", "w")
-
-
-def log(*args, **kwargs):
-    print(*args, **kwargs, file=_f, flush=True)
