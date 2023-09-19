9050c645fc3fda3658c347727374cea0d64d0d57
jqxcz <jqxczz@gmail.com>
2023-09-19 13:10:33 +1000

    Make Haskell script for gate synthesis

---
+++ gate_synth.hs
@@ -0,0 +1,20 @@
+import Quantum.Synthesis.GridSynth
+import Quantum.Synthesis.SymReal
+import System.Random
+import System.IO
+
+
+gate_synth :: IO ()
+gate_synth = do
+    input <- getLine
+    let (p:q:precision:effort:seed:xs) = map read(words input) :: [Int]
+
+    let angle = ( Pi * to_real p ) / to_real q
+
+    print(gridsynth_gates(mkStdGen seed)(fromIntegral precision)(angle)(effort))
+
+    gate_synth
+
+main :: IO ()
+main = do gate_synth
+    
