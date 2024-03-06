4a05ebefb8995b831076e9ccc5c2189ece94a3fc
Alan <arob8086@uni.sydney.edu.au>
2024-03-07 09:55:02 +1100

    Updated examples

--- examples/cnot/cnot_network.py
+++ examples/cnot/cnot_network.py
@@ -1,6 +1,7 @@
 import numpy as np
 from functools import partial
 from surface_code_routing.dag import DAG
+from surface_code_routing.symbol import Symbol
 from surface_code_routing.instructions import INIT, CNOT, MEAS, X, Hadamard
 from surface_code_routing.lib_instructions import T_Factory, Toffoli
 
@@ -27,6 +28,22 @@ def cnot_network(n_qubits, height, width, n_rounds = 1, teleport=True):
 
 def toff_network(n_qubits, height, width, *externs, n_rounds = 1, teleport=True):
     dag = DAG(f'CNOTS_{n_qubits}')
+    qubit_ordering = list(range(n_qubits))
+
+    # Pre-calc ordering to ensure seeding works correctly
+    gate_orderings = [np.random.permutation(qubit_ordering) for _ in range(n_rounds)]
+
+    for i in range(n_qubits):
+        dag.add_gate(INIT(f'q_{i}'))
+
+    for gate_round in gate_orderings:
+        for i, j, k in zip(gate_round[::3], gate_round[1::3], gate_round[2::3]):
+            dag.add_gate(Toffoli(f'q_{i}', f"q_{j}", f"q_{k}"))
+
+    return compile_qcb(dag, height, width, *externs, router_kwargs={'teleport':teleport}) 
+
+def route_example(n_qubits, height, width, *externs, n_rounds = 1, teleport=True):
+    dag = DAG(Symbol('a', 'b', 'c'))
     qubit_ordering = list(range(n_qubits))
 
     # Pre-calc ordering to ensure seeding works correctly
